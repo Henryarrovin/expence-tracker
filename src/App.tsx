d02ecc3d0914d8a6fd11c70a86870bc47442402b
Henry <henry.a.s.arrovin@gmail.com>
2024-04-20 13:52:39 +0530

    Created ExpenceList

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import "./App.css";
+import ExpenceList from "./components/ExpenceList";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 3, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 4, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 5, description: "aaa", amount: 10, category: "Utilities" },
+  ]);
+
+  const onDelete = (expenceId: number) => {
+    const updatedExpence = expenses.filter(
+      (expence) => expence.id !== expenceId
+    );
+    setExpenses(updatedExpence);
+  };
   return (
     <>
       <h1>Expence Tracker</h1>
+      <ExpenceList expenses={expenses} onDelete={onDelete} />
     </>
   );
 }
---
+++ src/components/ExpenceList.tsx
@@ -0,0 +1,60 @@
+interface Expence {
+  id: number;
+  description: string;
+  amount: number;
+  category: string;
+}
+
+interface Props {
+  expenses: Expence[];
+  onDelete: (id: number) => void;
+}
+const ExpenceList = ({ expenses, onDelete }: Props) => {
+  if (expenses.length === null) return null;
+  return (
+    <div>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Amount</th>
+            <th>Category</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense) => (
+            <tr key={expense.id}>
+              <td>{expense.description}</td>
+              <td>{expense.amount}</td>
+              <td>{expense.category}</td>
+              <td>
+                <button
+                  className="btn btn-outline-danger"
+                  onClick={() => onDelete(expense.id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td>Total</td>
+            <td>
+              $
+              {expenses
+                .reduce((acc, expence) => expence.amount + acc, 0)
+                .toFixed(2)}
+            </td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
+};
+
+export default ExpenceList;
